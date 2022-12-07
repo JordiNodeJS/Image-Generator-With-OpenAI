c3145df10d5e6e079205da428ed6f9577455c4fe
JORDI.NODEJS <jordi.nodejs@gmail.com>
2022-12-07 23:27:03 +0100

    test form post with postman

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const port = process.env.PORT || 3000
 
 const app = express()
 
+// Enable to accept body data
+app.use(express.json())
+app.use(express.urlencoded({ extended: false}))
+
 app.use('/openai', require('./routes/openaiGenerate'))
 
 app.listen(port, () => console.log(`Server start on port ${port}`))
