3313d608eb43dd84a7dca324bf7ce0abb73d5f74
JORDI.NODEJS <jordi.nodejs@gmail.com>
2022-12-07 21:24:15 +0100

    start basic server

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('hi')
+const express = require('express')
+const dotenv = require('dotenv').config
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.listen(port, _ => console.log(`Server start on port ${port}`))
