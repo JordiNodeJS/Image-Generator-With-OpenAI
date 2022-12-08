f0d085a38fd12b4711485fd7186b6ebc31c5e8cf
JORDI.NODEJS <jordi.nodejs@gmail.com>
2022-12-08 22:13:54 +0100

    renaming routes

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ app.use(express.urlencoded({ extended: false }))
 // Public static files
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/openai', require('./routes/imageGenerateByOpenAI'))
+app.use('/openai', require('./routes/imageGenerator'))
 
 app.listen(port, () => console.log(`Server start on port ${port}`))
